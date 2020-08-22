8f638631bdefa3dce5ddab95ef153399a938f43e
werliton <werliton.carlos@ceuma.br>
2020-08-21 22:54:19 -0300

    Finalizando ultimas rotas

--- src/app.js
+++ src/app.js
@@ -14,7 +14,7 @@ const repositories = [];
  * Lista todos os repositórios cadastrados
  */
 app.get("/repositories", (request, response) => {
-    return response.json(repositories)
+  return response.json(repositories)
 });
 
 /**
@@ -23,12 +23,12 @@ app.get("/repositories", (request, response) => {
 app.post("/repositories", (request, response) => {
   const { title, url, techs } = request.body
 
-  const repository = { 
+  const repository = {
     id: uuid(),
-    title, 
-    url, 
-    techs, 
-    likes: 0 
+    title,
+    url,
+    techs,
+    likes: 0
   }
 
   repositories.push(repository)
@@ -41,32 +41,63 @@ app.put("/repositories/:id", (request, response) => {
   const { id } = request.params
   const { title, url, techs } = request.body
 
-  const update = repositories.find(item => {
-    if (item.id === id) {
-      const updated = {
-        item
-      }
-    }
-  })
+  const index = repositories.findIndex(item => item.id === id)
+
+  if (index === -1) {
+    return response.status(400).json({
+      message: 'Repository not exist'
+    })
+  }
+
+  const repositoryUpated = {
+    id,
+    url,
+    title,
+    techs, 
+    likes: repositories[index].likes
+  }
+
+  repositories[index] = repositoryUpated
 
 
+  return response.json(repositories[index])
 
 });
 
+/**
+ * Deletando um repositorio especifico
+ */
 app.delete("/repositories/:id", (request, response) => {
   const { id } = request.params
 
   const indexDeleted = repositories.findIndex(item => item.id === id)
 
-  if (indexDeleted > 0) {
+  if (indexDeleted > -1) {
     repositories.splice(indexDeleted, 1)
+  } else {
+    return response.status(400).json({
+      message: 'Repository not exist'
+    })
   }
 
   return response.status(204).json({})
+
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params
+
+  const index = repositories.findIndex(item => item.id === id)
+
+  if (index === -1) {
+    return response.status(400).json({
+      error:' Repository not exist'
+    })
+  }
+
+  repositories[index].likes += 1 
+   
+  return response.json(repositories[index])
 });
 
 module.exports = app;
